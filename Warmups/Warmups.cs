c1db22da24cc94ca165272f774e9800fe1d2afc2
Dean Choi <deanchoi@gmail.com>
2015-09-17 13:44:33 -0400

    Initial copy

---
+++ Warmups/Warmups.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warmups
+{
+    public class Warmup
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Dean's Strings Warmups:\n");
+            SayHi("Dean");
+            Abba("Hello", "Goodbye");
+            MakeTags("tag", "Things");
+            InsertWord("<<>>", "YOLO");
+            MultipleEndings("Hello");
+
+            Console.ReadLine();
+
+        }
+
+        public static void SayHi(string name)
+        {
+            Console.WriteLine("Hello {0}!", name);
+        }
+
+        public static void Abba(string a, string b)
+        {
+            Console.WriteLine("{0}{1}{1}{0}", a, b);
+        }
+
+        public static void MakeTags(string tag, string content)
+        {
+            Console.WriteLine("<{0}>{1}</{0}>", tag, content);
+        }
+
+        public static void InsertWord(string container, string word)
+        {
+            Console.WriteLine("{0}{1}{4}{2}{3}", container[0], container[1], container[2], container[3], word);
+        }
+
+        public static void MultipleEndings(string str)
+        {
+            Console.WriteLine("{0}{1}{0}{1}{0}{1}", str[str.Length - 2], str[str.Length - 1]);
+
+        }
+    }
+}
+
---
+++ WarmupsTests/WarmupsTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Warmups;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace WarmupsTests
+{
+    public class WarmupsTests
+    {
+        private Warmup _warmup;
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            _warmup = new Warmup();
+            Console.WriteLine("SetUp Called.");
+        }
+
+        [Test]
+        public void TestingString(string inputString, string expectedString)
+        {
+            string resultString = _warump.SayHi(inputString);
+            Assert.AreEqual(resultString, expectedString);
+
+        }
+        [TestCase("Dean", "Hello Dean!")]
+
+
+
+       
+    }
+}
